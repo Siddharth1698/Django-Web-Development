75af0618285e93ec35c4179ccf6099a58ca80a03
Siddharth M <siddharth.m98@gmail.com>
2019-05-13 20:24:59 +0530

    playing...

---
+++ HTML Part 2/JavaScript/consolePlay.js
@@ -0,0 +1,32 @@
+3
+3
+3+5
+8
+"hai"
+"hai"
+"hai"+" ok"
+"hai ok"
+"ok" + " google"
+"ok google"
+"okgoogle".length
+8
+"okgoogle"[1]
+"k"
+var mon = 10
+undefined
+var mon = 10;
+undefined
+var don = 10;
+undefined
+mon
+10
+don
+10
+var fon
+undefined
+fon = mon + don
+20
+var age = prompt("enter ur age")
+undefined
+age
+"90"
