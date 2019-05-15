b318ec9934d9f5bb559b9a37c3b79b02c1c9da43
Siddharth M <siddharth.m98@gmail.com>
2019-05-15 15:34:48 +0530

    strings in python

---
+++ Python/strings.py
@@ -0,0 +1,16 @@
+"hello world"
+'hello world'
+"i'm good"
+
+my_string = "abcdefghij"
+print(my_string)
+print(my_string[1])
+print(my_string[2:])
+print(my_string[:2])
+print(my_string[2:5])
+print(my_string[::2])
+
+x = my_string.upper()
+print(x)
+
+print("hello world the text is {}".format("nothing"))
