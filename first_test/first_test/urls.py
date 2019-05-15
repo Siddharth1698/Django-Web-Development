0370d9e161e5b9cee106ca345f98ea577beecf9e
Siddharth M <siddharth.m98@gmail.com>
2019-05-15 19:19:04 +0530

    Succesfully deployed locally

--- first_test/first_test/urls.py
+++ first_test/first_test/urls.py
@@ -1,4 +1,4 @@
-"""first_project URL Configuration
+"""first_test URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.2/topics/http/urls/
