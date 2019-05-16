8ce7fe9a89a2a2c27e069126b153d0026421e961
Siddharth M <siddharth.m98@gmail.com>
2019-05-16 13:34:59 +0530

    changing directories for template

--- mysite/first_app/views.py
+++ mysite/first_app/views.py
@@ -4,6 +4,6 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    my_dict = {'insert_me' : "hello im views.py!"}
-    return render(request,'index.html',context = my_dict)
+    my_dict = {'insert_me' : "hello ive changed .py!"}
+    return render(request,'mysite/index.html',context = my_dict)
       
